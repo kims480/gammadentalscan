997d2778839962199a1a049610d40c42ae25197f
kims480 <eng.karim@2010gmail.com>
2020-12-25 17:18:14 +0400

    updating iconmind-fonts5

--- resources/js/services/ServicesConst.js
+++ resources/js/services/ServicesConst.js
@@ -1,7 +1,7 @@
 /** @format */
 import { mapGetters, mapState } from "vuex";
 import axios from "axios";
-const myBaseURL = "https://gamma-dental-scan.com";
+const myBaseURL = "https://gamma-dental-scan.dev";
 const myBaseURLPort = "/";
 const myFullBaseURL = myBaseURL + myBaseURLPort;
 const myApiBaseURL = myFullBaseURL + 'api';
