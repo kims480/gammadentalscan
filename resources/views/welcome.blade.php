221e87d681394bf75dc80f3522e47e604433a1bd
kims480 <eng.karim@2010gmail.com>
2020-12-25 17:48:18 +0400

    updating iconmind-fonts6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
     <!-- Fonts -->
 
     <link rel="stylesheet" href="{{ asset('css/lite-green.css') }}" />
+    <link rel="stylesheet" href="{{ asset('fonts/iconsmind.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <script src="{{ asset('js/app.js') }}" defer></script>
 
