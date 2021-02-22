4e84996cc7bc7f88d864366a5d6e3215f3f7d0db
kims480 <eng.karim2010@gmail.com>
2021-02-22 20:38:35 +0400

    update request-view-102

--- resources/js/components/AppB.vue
+++ resources/js/components/AppB.vue
@@ -129,7 +129,7 @@ export default {
         },
       ],
       base: {
-        href: "https://gamma-dental-scan.dev/dashboard",
+        href: "https://gamma-dental-scan.com/dashboard",
         target: "_Self",
       },
       bodyAttrs: {
