133fa270d9a4d0d77c3a65623427e9149ca9f45e
kims480 <eng.karim2010@gmail.com>
2021-03-02 22:24:03 +0400

    update signin

--- resources/js/pages/auth/signin.vue
+++ resources/js/pages/auth/signin.vue
@@ -59,25 +59,18 @@
                 @click.prevent="login"
               >
                 SIGN IN
-                <template v-slot:loader>
-                  <span class="custom-loader">
-                    <v-icon light>mdi-cached</v-icon>
-                  </span>
-                </template>
+                <v-progress-circular
+                  v-if="loading"
+                  :size="50"
+                  color="primary"
+                  indeterminate
+                ></v-progress-circular>
               </v-btn>
               <hr class="my-4" />
               <span>
                 Donot have Account! Need to
                 <router-link to="/register">Register</router-link>
               </span>
-
-              <hr class="my-4" />
-              <button class="btn btn-lg btn-google btn-block text-uppercase">
-                <i class="fab fa-google mr-2"></i> Sign in with Google
-              </button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase">
-                <i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook
-              </button>
             </form>
           </div>
         </div>
